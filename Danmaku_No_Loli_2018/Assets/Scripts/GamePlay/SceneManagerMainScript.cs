96d6bf788ba3c6c9fa98c65ef5b6ee891726beeb
sorlinV <valentin.sorlin@hotmail.com>
2017-12-17 14:41:57 +0100

    adding credit system

--- Danmaku_No_Loli_2018/Assets/Scripts/GamePlay/SceneManagerMainScript.cs
+++ Danmaku_No_Loli_2018/Assets/Scripts/GamePlay/SceneManagerMainScript.cs
@@ -1,21 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManagerMainScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void OnLevelWasLoaded(int level) {
-
-		Time.timeScale = 1;
-	}
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
 }
